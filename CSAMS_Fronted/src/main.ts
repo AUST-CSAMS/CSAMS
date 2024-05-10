451cd3864421df830b546ede70c7ad90b3ea6eb4
rzz <1790146932@qq.com>
2024-05-10 16:03:42 +0800

    初始化

---
+++ CSAMS_Fronted/src/main.ts
@@ -0,0 +1,16 @@
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import ArcoVue from '@arco-design/web-vue';
+import App from './App.vue'
+import router from './router'
+import '@arco-design/web-vue/dist/arco.css';
+import "@/assets/theme.css"
+import "@/assets/base.css"
+
+const app = createApp(App)
+app.use(ArcoVue)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
