fe7dc10a8788cbbde7183d99ed87667f9df7baf1
rzz <1790146932@qq.com>
2024-05-17 16:22:43 +0800

    初始化

--- CSAMS-Frontend/src/main.ts
+++ CSAMS-Frontend/src/main.ts
@@ -13,3 +13,18 @@ app.use(router);
 
 
 app.mount('#app')
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import ArcoVue from '@arco-design/web-vue';
+import App from './App.vue'
+import router from './router'
+import '@arco-design/web-vue/dist/arco.css';
+import "@/assets/base.css"
+import "font-awesome/css/font-awesome.min.css"
+const app = createApp(App)
+app.use(ArcoVue)
+app.use(createPinia())
+app.use(router);
+
+
+app.mount('#app')
--- CSAMS-Frontend/src/router/index.ts
+++ CSAMS-Frontend/src/router/index.ts
@@ -7,3 +7,12 @@ const router = createRouter({
 })
 
 export default router
+import {createRouter, createWebHistory} from 'vue-router'
+
+const router = createRouter({
+    //createWebHistory 创建HTML5历史记录
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: []
+})
+
+export default router
