0c34203ac6cb250ad6bb33e465e3ba4a19f867e3
scc749 <1262376955@qq.com>
2024-06-03 15:28:30 +0800

    活动创建接口和图片上传接口完成

--- CSAMS-Backend/routers/activity_router.go
+++ CSAMS-Backend/routers/activity_router.go
@@ -1,10 +1,14 @@
 package routers
 
-import "CSAMS-Backend/api"
+import (
+	"CSAMS-Backend/api"
+	"CSAMS-Backend/middleware"
+)
 
 func (router RouterGroup) ActivityRouter() {
 	app := api.ApiGroupApp.ActivityApi
 	router.GET("activities/finished", app.ActivityFinishedListView)
 	router.GET("activities/ongoing", app.ActivityOngoingListView)
 	router.GET("activities/upcoming", app.ActivityUpcomingListView)
+	router.POST("activities", middleware.JwtTeacher(), app.ActivityCreateView)
 }
