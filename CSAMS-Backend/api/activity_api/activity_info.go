08542a078825d859fcb1481eac40b40944087986
scc749 <1262376955@qq.com>
2024-06-06 13:41:46 +0800

    活动信息路由，bug修改

---
+++ CSAMS-Backend/api/activity_api/activity_info.go
@@ -0,0 +1,28 @@
+package activity_api
+
+import (
+	"CSAMS-Backend/global"
+	"CSAMS-Backend/models"
+	"CSAMS-Backend/models/res"
+	"github.com/gin-gonic/gin"
+)
+
+type ActivityInfoRequest struct {
+	ID uint64 `json:"id" bind:"required"` // 活动id
+}
+
+func (ActivityApi) ActivityInfoView(c *gin.Context) {
+	var cr ActivityInfoRequest
+	if err := c.ShouldBindJSON(&cr); err != nil {
+		res.FailWithCode(res.ArgumentError, c)
+		return
+	}
+
+	var activityInfo models.ActivityModel
+	err := global.DB.Take(&activityInfo, cr.ID).Error
+	if err != nil {
+		res.FailWithMessage("活动不存在", c)
+		return
+	}
+	res.OkWithData(activityInfo, c)
+}
