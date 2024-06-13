11147b003eedf23bbed4ca8d04aabeced84bece4
scc749 <1262376955@qq.com>
2024-06-13 22:11:49 +0800

    bug修复

--- CSAMS-Backend/api/association_api/association_quit.go
+++ CSAMS-Backend/api/association_api/association_quit.go
@@ -3,6 +3,7 @@ package association_api
 import (
 	"CSAMS-Backend/global"
 	"CSAMS-Backend/models"
+	"CSAMS-Backend/models/ctype"
 	"CSAMS-Backend/models/res"
 	"CSAMS-Backend/utils/jwts"
 	"github.com/gin-gonic/gin"
@@ -33,12 +34,31 @@ func (AssociationApi) AssociationQuitView(c *gin.Context) {
 	}
 
 	for _, id := range cr.IDs {
+		if id == associationModel.TeacherID {
+			res.FailWithMessage("不能将教师踢出协会", c)
+			continue
+		}
+
 		var associationMember models.AssociationMemberModel
 		err = global.DB.Take(&associationMember, id).Error
 		if err != nil {
 			print(err)
 			res.FailWithMessage("成员查找失败", c)
 		}
+
+		if associationMember.Posts == "会长" {
+			err := global.DB.Model(&associationModel).Update("president", nil).Error
+			if err != nil {
+				res.FailWithMessage(err.Error(), c)
+				return
+			}
+			err = global.DB.Model(&models.UserModel{}).Where("id = ?", id).Update("role", ctype.PermissionStudent).Error
+			if err != nil {
+				res.FailWithMessage(err.Error(), c)
+				return
+			}
+		}
+
 		err = global.DB.Delete(&associationMember).Error
 		if err != nil {
 			print(err)
