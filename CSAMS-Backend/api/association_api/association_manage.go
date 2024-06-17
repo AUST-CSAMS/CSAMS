dc37890cb18be96243083f922ea20d7dcf8efea6
scc749 <1262376955@qq.com>
2024-06-17 14:46:52 +0800

    bug修复

--- CSAMS-Backend/api/association_api/association_manage.go
+++ CSAMS-Backend/api/association_api/association_manage.go
@@ -24,6 +24,11 @@ func (AssociationApi) AssociationManageView(c *gin.Context) {
 	_claims, _ := c.Get("claims")
 	claims := _claims.(*jwts.CustomClaims)
 
+	if cr.ID == claims.UserID {
+		res.FailWithMessage("不能修改负责教师信息", c)
+		return
+	}
+
 	var association models.AssociationModel
 
 	err := global.DB.Take(&association, "teacher_id = ?", claims.UserID).Error
@@ -32,18 +37,15 @@ func (AssociationApi) AssociationManageView(c *gin.Context) {
 		res.FailWithMessage("还未创建协会", c)
 		return
 	}
-	var associationMember models.AssociationMemberModel
-	// 使用 db.Update 方法进行更新
-	err = global.DB.Take(&associationMember, cr.ID).Update("Posts", cr.Posts).Error
-	if err != nil {
-		print(err)
-		res.FailWithMessage("成员职位修改失败", c)
-	}
-	res.OkWithMessage("成员职位修改成功", c)
 
 	if cr.Posts == "会长" {
+		err := global.DB.Take(&models.AssociationMemberModel{}, "posts = ?", cr.Posts).Error
+		if err == nil {
+			res.FailWithMessage("已存在会长", c)
+			return
+		}
 		var user models.UserModel
-		err := global.DB.Take(&user, cr.ID).Update("role", ctype.PermissionStudentAdmin).Error
+		err = global.DB.Take(&user, cr.ID).Update("role", ctype.PermissionStudentAdmin).Error
 		if err != nil {
 			res.FailWithMessage("用户不存在", c)
 			return
@@ -55,4 +57,28 @@ func (AssociationApi) AssociationManageView(c *gin.Context) {
 		}
 		res.OkWithMessage("会长信息同步成功", c)
 	}
+
+	var associationMember models.AssociationMemberModel
+	err = global.DB.Take(&associationMember, cr.ID).Error
+	if err != nil {
+		print(err)
+		res.FailWithMessage("未找到成员信息", c)
+	}
+	if associationMember.Posts == "会长" && cr.Posts == "成员" {
+		err = global.DB.Model(&association).Update("president", nil).Error
+		if err != nil {
+			print(err)
+			res.FailWithMessage("协会会长信息删除失败", c)
+		}
+		res.OkWithMessage("协会会长信息删除成功", c)
+	}
+
+	// 使用 db.Update 方法进行更新
+	err = global.DB.Model(&associationMember).Update("posts", cr.Posts).Error
+	if err != nil {
+		print(err)
+		res.FailWithMessage("成员职位修改失败", c)
+	}
+	res.OkWithMessage("成员职位修改成功", c)
+
 }
